b2744f574b6bb8bd57b878ab0db4bd7c0d6e6fbc
xxuxa-k <tk0x2bc@gmail.com>
2022-06-01 22:31:23 +0900

    tmp commit

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	// "context"
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 	// "net/http"
-	// "google.golang.org/api/calendar/v3"
+	"google.golang.org/api/calendar/v3"
+	// "google.golang.org/api/option"
 	// "google.golang.org/api/transport"
 )
 
@@ -15,9 +16,14 @@ func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println(".env can't be loaded.")
+		return
 	}
-	API_KEY := os.Getenv("API_KEY")
-	if API_KEY != "" {
-		fmt.Println(API_KEY)
+
+	ctx := context.Background()
+	calendarService, err := calendar.NewService(ctx)
+	if err != nil {
+		fmt.Println("Calendar service initialize error.")
 	}
+	eventList := calendarService.Events.List(os.Getenv("CALENDAR_ID"))
+	fmt.Println(eventList)
 }
